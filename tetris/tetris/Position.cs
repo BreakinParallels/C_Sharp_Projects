37e377feeff503fb7218ef9eb54440686933fea2
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2022-07-19 21:19:02 -0400

    add tetris

---
+++ tetris/tetris/Position.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace tetris
+{
+    public class Position
+    {
+        public int Row { get; set; }
+
+        public int Column { get; set; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
